cad117dc382e2af85eaf8f0b8019958990118759
omarazhar10 <omar10.azhar@gmail.com>
2025-01-07 08:52:38 +0500

    L7 of bootstrap

---
+++ Lesson 19 (javascript)/Functions.js
@@ -0,0 +1,22 @@
+/*
+//eXAMPLE 1|\\
+function groceryList()
+    console.log("Butter")
+    console.log("Milk")
+    console.log("Chocolate")
+    console.log("Ice cream")
+    console.log("Chips")
+    console.log("Potato")
+    console.log("Tomato")
+    console.log("Onion")
+}
+groceryList()
+console.log("I want to go to the mart for grocery.")
+*/
+
+function greet(name,age){
+    console.log("hello", name)
+    console.log("your age is", age)
+}
+
+greet("omar", 14)
